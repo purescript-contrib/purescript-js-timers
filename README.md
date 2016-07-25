93d71a21452f864c50629491ce8b9a319ffe88d7
Gary Burgess <gary.burgess@gmail.com>
2016-07-25 13:13:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# purescript-js-timers
+
+[![Latest release](http://img.shields.io/bower/v/purescript-js-timers.svg)](https://github.com/purescript-contrib/purescript-js-timers/releases)
+[![Build Status](https://travis-ci.org/purescript-contrib/purescript-js-timers.svg?branch=master)](https://travis-ci.org/purescript-contrib/purescript-js-timers)
+[![Maintainer: garyb](https://img.shields.io/badge/maintainer-garyb-lightgrey.svg)](http://github.com/garyb)
+
+Low level bindings for JavaScript's timers API.
+
+## Installation
+
+```
+bower install purescript-js-timers
+```
+
+## Documentation
+
+Module documentation is [published on Pursuit](http://pursuit.purescript.org/packages/purescript-js-timers).
---
+++ src/Control/Monad/Eff/Timer.purs
@@ -0,0 +1,65 @@
+module Control.Monad.Eff.Timer
+  ( TIMER
+  , TimeoutId
+  , setTimeout
+  , clearTimeout
+  , IntervalId
+  , setInterval
+  , clearInterval
+  ) where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+
+-- | The effect for the usage of a JavaScript timer.
+foreign import data TIMER :: !
+
+-- | The ID of a timer started with `setTimeout`.
+newtype TimeoutId = TimeoutId Int
+
+derive instance eqTimeoutId :: Eq TimeoutId
+derive instance ordTimeoutId :: Ord TimeoutId
+
+-- | Runs an effectful function after the specified delay in milliseconds. The
+-- | returned `TimeoutId` can be used to cancel the timer before it completes.
+-- |
+-- | The timeout delay value is capped at 4ms by the JS API, any value less than
+-- | this will be clamped.
+foreign import setTimeout
+  :: forall eff
+   . Int
+  -> Eff (timer :: TIMER | eff) Unit
+  -> Eff (timer :: TIMER | eff) TimeoutId
+
+-- | Cancels a timeout. If the timeout has already been cancelled or has already
+-- | elapsed this will have no effect.
+foreign import clearTimeout
+  :: forall eff
+   . TimeoutId
+  -> Eff (timer :: TIMER | eff) Unit
+
+-- | The ID of a timer started with `setInterval`.
+newtype IntervalId = IntervalId Int
+
+derive instance eqIntervalId :: Eq IntervalId
+derive instance ordIntervalId :: Ord IntervalId
+
+-- | Runs an effectful function after on a set interval with the specified delay
+-- | in milliseconds between iterations. The returned `IntervalId` can be used
+-- | to cancel the timer and prevent the interval from running any further.
+-- |
+-- | The interval delay value is capped at 4ms by the JS API, any value less
+-- | than this will be clamped.
+foreign import setInterval
+  :: forall eff
+   . Int
+  -> Eff (timer :: TIMER | eff) Unit
+  -> Eff (timer :: TIMER | eff) IntervalId
+
+-- | Cancels an interval timer. If the interval has already been cancelled this
+-- | will have no effect.
+foreign import clearInterval
+  :: forall eff
+   . IntervalId
+  -> Eff (timer :: TIMER | eff) Unit
